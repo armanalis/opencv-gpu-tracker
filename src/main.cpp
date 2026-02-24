f6c4ac066354e41e3be17bb1494a6e000245a160
Ali <s309299@studenti.polito.it>
2026-02-24 13:53:42 +0300

    Processed and enhanced image demonstration.

--- src/main.cpp
+++ src/main.cpp
@@ -11,35 +11,40 @@ int main(){
         return -1;
     }
 
+    cv::Ptr<cv::CLAHE> clahe = cv::createCLAHE(3.0, cv::Size(8, 8));
+
     cv::namedWindow("Video", cv::WINDOW_NORMAL);
-    cv::Mat frame;
+    cv::UMat frame, grayFrame, enhancedFrame;
 
     while(true){
 
         bool ok = cap.read(frame);
 
-        if (!ok)
+        if (!ok)//checking if the read has failed or not
         {
             std::cout << "read() failed\n";
             break;
         }
 
-        if (frame.empty())
+        if (frame.empty())//checking if the frame is empty or not
         {
             std::cout << "frame empty\n";
             break;
         }
 
-        std::cout << "Frame OK: "
-                  << frame.cols << " x "
-                  << frame.rows << std::endl;
+        //changing to gray tone
+        cv::cvtColor(frame, grayFrame, cv::COLOR_BGR2GRAY);
 
-        cv::imshow("Video", frame);
+        //usage of CLAHE
+        clahe->apply(grayFrame, enhancedFrame);
 
-        // macOS için minimum 1 ms gerekli
+        //Show the original and processed image side by side
+        cv::imshow("Original IHA cam", frame);
+        cv::imshow("T-API Enchanced Frame (CLAHE)", enhancedFrame);
+
         int key = cv::waitKey(1);
 
-        if (key == 'q')
+        if (key == 27)//ESC key to quit
             break;
         }
 
